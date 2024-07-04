35769c0f6861d3b88b1ea0272b6d7e0e2a51665d
gloriacullelldeveloper <gcullelldev@gmail.com>
2024-07-04 17:14:57 +0200

    Added the current location of each person when they clicked "My location" option with JS

--- src/index.js
+++ src/index.js
@@ -58,6 +58,10 @@ italyTimeElement.innerHTML = italyTime.format("h:mm:ss [<small>]A[</small>]");
 function updateCity(event) {
 
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
